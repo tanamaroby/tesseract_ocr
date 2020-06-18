4368bb5d5a016eb782bf5b5bb670adb023fe72c8
robytanama <tanamaroby@gmail.com>
2020-06-18 18:08:45 +0800

    Separate preprocessor into independent class

--- tesseract_ocr.py
+++ tesseract_ocr.py
@@ -1,66 +1,14 @@
-# Imports
+# Library imports
 import cv2
 import pytesseract
 import numpy as np
 
-# Path to image file
-img = cv2. imread('images/example_01.png')
+# Self-defined classes imports
+import preprocessor
 
-# Pre-processing methods
-# Get grayscale images
-def get_grayscale(image):
-    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-# Noise removal
-def remove_noise(image):
-    return cv2.medianBlur(image, 5)
-
-# Thresholding
-def thresholding(image):
-    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-
-# Dilation
-def dilate(image):
-    kernel = np.ones((5, 5), np.uint8)
-    return cv2.dilate(image, kernel, iterations = 1)
-
-# Erosion
-def erode(image):
-    kernel = np.ones((5, 5), np.uint8)
-    return cv2.erode(image, kernel, iterations = 1)
-
-# Opening - erosion followed by dilation
-def opening(image):
-    kernel = np.ones((5,5), np.uint8)
-    return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel) 
-
-# Canny edge detection
-def canny(image):
-    return cv2.Canny(image, 100, 200)
-
-# Skew correction
-def deskew(image):
-    coords = np.column_stack(np.where(image > 0))
-    angle = cv2.minAreaRect(coords)[-1]
-    if angle < -45:
-        angle = -(90 + angle)
-    else:
-        angle = -angle
-    (h, w) = image.shape[:2]
-    center = (w // 2, h // 2)
-    M = cv2.getRotationMatrix2D(center, angle, 1.0)
-    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-    return rotated
-
-# Template matching
-def match_template(image, template):
-    return cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
-
-# Pre-process wrapper function (edit accordingly)
-def pre_process(image):
-    image = get_grayscale(image)
-    image = thresholding(image)
-    return image
+# Initialize tools
+img = cv2.imread('images/example_01.png') # Path to image file
+pre_processor = preprocessor.preprocessor() # Pre-processor
 
 # Show pre-processed image output for debugging
 def show_image(image):
@@ -68,7 +16,7 @@ def show_image(image):
     cv2.waitKey(0)
 
 # Process from image to string and print out
-preprocessed_image = pre_process(img)
+preprocessed_image = pre_processor.pre_process(img)
 output = pytesseract.image_to_string(preprocessed_image)
 print(output)
 
