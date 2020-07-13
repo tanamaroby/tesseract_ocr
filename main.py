d6b8c3f083c286bb7f01b7be603857a2fdaa5445
JeremyKwok <jeremykke@gmail.com>
2020-07-13 17:23:38 +0800

    add image cropping

--- main.py
+++ main.py
@@ -10,8 +10,20 @@ import imageboxer
 import imageorientation
 import idprocessor
 
+def crop_left_half(image):
+    cropped_img = image[:, 0:image.shape[0]//2]
+    return cropped_img
+def crop_bottom_half(image):
+    cropped_img = image[image.shape[0]//2:image.shape[0]]
+    return cropped_img
+def crop_top_half(image):
+    cropped_img = image[0:image.shape[0]//2]
+    return cropped_img
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
 # Converting PDF to PNG when necessary
-filepath = 'documents/PANCard3.jpeg'
+filepath = 'documents/PANCard.png'
 # Checking if the file given is PDF or Image format
 if ('.pdf' in filepath):
     # In the case of multiple pages
@@ -38,7 +50,8 @@ output_postprocessor = idprocessor.idprocessor() # Process the output
 # Process from image to string and write into file
 preprocessed_image = image_preprocessor.preprocess(img)
 boxed_image = image_boxer.box_image(img)
-output = pytesseract.image_to_string(preprocessed_image, lang='eng')
+preprocessed_image=crop_top_half(preprocessed_image)
+output = pytesseract.image_to_string(preprocessed_image, lang='eng', config='--psm 6')
 
 # Processing the output
 processedoutput = output_postprocessor.postprocess(output)
@@ -56,3 +69,4 @@ with open('generated/text/processedoutput.txt', 'w+') as file:
 # Save preprocessed images for debugging
 cv2.imwrite("generated/images/preprocessed_image.png", preprocessed_image)
 cv2.imwrite("generated/images/boxed_image.png", boxed_image)
+
