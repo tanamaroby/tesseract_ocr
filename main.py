0676f7cee962f9ef9d9e6837e41d85751050d118
robytanama <tanamaroby@gmail.com>
2020-07-14 09:19:28 +0800

    modify crop.py to be run from the main method

--- main.py
+++ main.py
@@ -9,21 +9,13 @@ import imagepreprocessor
 import imageboxer
 import imageorientation
 import idprocessor
-
-def crop_left_half(image):
-    cropped_img = image[:, 0:image.shape[0]//2]
-    return cropped_img
-def crop_bottom_half(image):
-    cropped_img = image[image.shape[0]//2:image.shape[0]]
-    return cropped_img
-def crop_top_half(image):
-    cropped_img = image[0:image.shape[0]//2]
-    return cropped_img
+import crop
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 # Converting PDF to PNG when necessary
-filepath = 'documents/PANCard.png'
+filepath = 'documents/PANCard3.jpeg'
+
 # Checking if the file given is PDF or Image format
 if ('.pdf' in filepath):
     # In the case of multiple pages
@@ -44,13 +36,19 @@ else:
 image_preprocessor = imagepreprocessor.imagepreprocessor() # Pre-processor
 image_boxer = imageboxer.imageboxer() # Boxer
 image_orientation = imageorientation.imageorientation() # Detect orientation
+image_cropper = crop.crop() # Cropping tool
 
 output_postprocessor = idprocessor.idprocessor() # Process the output
 
-# Process from image to string and write into file
-preprocessed_image = image_preprocessor.preprocess(img)
-boxed_image = image_boxer.box_image(img)
-preprocessed_image=crop_top_half(preprocessed_image)
+
+# Crop the image
+cropped_image = image_cropper.crop(img) 
+
+# Process from image to string 
+preprocessed_image = image_preprocessor.preprocess(cropped_image)
+boxed_image = image_boxer.box_image(cropped_image)
+
+
 output = pytesseract.image_to_string(preprocessed_image, lang='eng', config='--psm 6')
 
 # Processing the output
@@ -69,4 +67,5 @@ with open('generated/text/processedoutput.txt', 'w+') as file:
 # Save preprocessed images for debugging
 cv2.imwrite("generated/images/preprocessed_image.png", preprocessed_image)
 cv2.imwrite("generated/images/boxed_image.png", boxed_image)
+cv2.imwrite("generated/images/cropped_image.png", cropped_image)
 
