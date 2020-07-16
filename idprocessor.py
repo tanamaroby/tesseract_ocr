9ae08128cdfd372b41bc50e9d79169285b1153e1
JeremyKwok <jeremykke@gmail.com>
2020-07-16 09:04:57 +0800

    restructure postprocess and return in dictionary

--- idprocessor.py
+++ idprocessor.py
@@ -31,11 +31,30 @@ class idprocessor:
             return False
 
     def postprocess(self, input):
-        name = self.namevalidator(input)
-        pannumber = self.pannumbervalidator(input)
-        dateofbirth = self.datevalidator(input)
-        
-        output = "Your name is: " + name + "\n\n"
-        output += "Your PAN Number is: " + pannumber + "\n\n"
-        output += "Your date of birth is: " + dateofbirth
-        return output
+        processed_id = dict()
+        name = False
+        pan_number = False
+        dob = False
+        inputlines = input.splitlines()
+        # Go through the input line by line
+        for line in inputlines:
+            # Only concerns upper case lines
+            if not pan_number and self.hasnumbers(line):
+                pan_number = self.pannumbervalidator(line)
+                processed_id['pan_number'] = pan_number
+            if line.isupper():
+                # Removing common keyword for the PAN Card titles
+                bannedwords = ["INCOME", "TAX", "DEPARTMENT", "GOVT", "OF", "INDIA", "Permanent", "Account", "Number"]
+                if not any(word in line for word in bannedwords):
+                    if not name:
+                        name = self.namevalidator(line)
+                        processed_id['name'] = name
+            elif not dob and self.hasnumbers(line):
+                dob = self.datevalidator(line)
+                processed_id['dob'] = dob
+
+        output = "Your name is: " + (name if name else "Not Found") + "\n\n"
+        output += "Your PAN Number is: " + (pan_number if pan_number else "Not Found") + "\n\n"
+        output += "Your date of birth is: " + (dob if dob else "Not Found")
+        print(processed_id)
+        return output, processed_id
